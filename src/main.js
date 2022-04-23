32375d7f36e240fbf15ed5c422536487e9c92e2b
CryDeTaan <CryDeTaan@users.noreply.github.com>
2022-04-23 13:47:39 +0100

    ✨ Use vue-router in app.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import {createRouter, createWebHistory } from 'vue-router'
 
-createApp(App).mount('#app')
+import PageHome from "@/components/PageHome";
+
+const routes = [
+    { path: '/', component: PageHome}
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+createApp(App)
+    .use(router)
+    .mount('#app')
