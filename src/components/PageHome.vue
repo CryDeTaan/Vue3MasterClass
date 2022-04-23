ad1a2e32b67e174a75102fec65b22c3308711c29
CryDeTaan <CryDeTaan@users.noreply.github.com>
2022-04-23 12:03:13 +0100

    🚧 Add some data to template.

--- src/components/PageHome.vue
+++ src/components/PageHome.vue
@@ -1,10 +1,36 @@
 <template>
-<div>Hello form Home page</div>
+  <div>Threads</div>
+  <div v-for="thread in threads" :key="thread.id">
+    <h2>{{ thread.title }}</h2>
+    <div v-for="postId in thread.posts" :key="postId">
+      <p>{{ userById(postById(postId).userId).name }}</p>
+      <p>{{ postById(postId).text }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
+import sourceData from '@/data.json'
+
 export default {
-  name: "PageHome"
+  name: "PageHome",
+
+  data() {
+    return {
+      threads: sourceData.threads,
+      posts: sourceData.posts,
+      users: sourceData.users,
+    }
+  },
+
+  methods: {
+    postById(postId) {
+      return this.posts.find(p => p.id === postId)
+    },
+    userById(userId) {
+      return this.users.find(p => p.id === userId)
+    }
+  },
 }
 </script>
 
