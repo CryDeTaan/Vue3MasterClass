35ab5d4ffb910b875dd2b76f56a445a68460e559
CryDeTaan <CryDeTaan@users.noreply.github.com>
2022-04-24 08:57:10 +0100

    ✨ Implement page not found and routing

---
+++ src/components/PageNotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="col-full text-center push-top">
+    <h1>404 - Not Found</h1>
+    <router-link :to="{name: 'Home'}" >Read some cool threads here...</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PageNotFound"
+}
+</script>
+
+<style scoped>
+
+</style>
