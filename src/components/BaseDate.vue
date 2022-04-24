1180a900f964af8424638f9be9e8be34ed58af66
CryDeTaan <CryDeTaan@users.noreply.github.com>
2022-04-24 12:17:03 +0100

    ✨ Create human friendly date component

---
+++ src/components/BaseDate.vue
@@ -0,0 +1,37 @@
+<template>
+  <span :title="humanFriendlyDateTime()">
+    {{ diffForHumans() }}
+  </span>
+</template>
+
+<script>
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+import localizedDate from 'dayjs/plugin/localizedFormat'
+dayjs.extend(relativeTime)
+dayjs.extend(localizedDate)
+
+export default {
+  name: "BaseDate",
+
+  props: {
+    timestamp: {
+      type: Number,
+      required: true,
+    },
+  },
+
+  methods: {
+    diffForHumans() {
+      return dayjs.unix(this.timestamp).fromNow()
+    },
+    humanFriendlyDateTime() {
+      return dayjs.unix(this.timestamp).format('llll')
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
