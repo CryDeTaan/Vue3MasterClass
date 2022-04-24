5894c96f4913dfbda068a2e4278b805da1abadeb
CryDeTaan <CryDeTaan@users.noreply.github.com>
2022-04-24 09:18:26 +0100

    🎨 Implement route guard for thread not found.

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import {createRouter, createWebHistory } from 'vue-router'
 import PageHome from "@/components/PageHome";
 import PageThreadShow from "@/components/PageThreadShow";
 import PageNotFound from "@/components/PageNotFound";
+import sourceData from "@/data.json";
 
 const routes = [
     {
@@ -14,7 +15,21 @@ const routes = [
         path: '/thread/:id',
         name: 'ThreadShow',
         component: PageThreadShow,
-        props: true
+        props: true,
+        beforeEnter(to, from, next) {
+            // check if the thread exists
+            const threadExists = sourceData.threads.find(thread => thread.id === to.params.id)
+            // if the thread exists, continue...
+            if (threadExists) return next();
+            // if the doesn't exist, navigate to 404 - Not found
+            next({
+                name: 'NotFound',
+                params: { pathMatch: to.path.substring(1).split('/')},
+                // preserve existing query and hash
+                query: to.query,
+                hash: to.hash
+            })
+        }
     },
     {
         path: '/:pathMatch(.*)*',
