6a63bff107b6d11484d4ce4bfbea4eaaca3d59e7
CryDeTaan <CryDeTaan@users.noreply.github.com>
2022-04-25 20:45:58 +0100

    ✨ Add category page.

---
+++ src/pages/Category.vue
@@ -0,0 +1,43 @@
+<template>
+  <h1>{{ category.name }}</h1>
+  <forum-list
+      :forums="getForumForCategory(category)"
+      :category="category"
+  />
+</template>
+
+<script>
+import ForumList from "@/components/ForumList";
+import sourceData from "@/data.json";
+
+export default {
+  name: "PageCategory",
+  components: {
+    ForumList
+  },
+
+  props: {
+    id: {
+      type: String,
+      required: true
+    },
+  },
+
+  computed: {
+    category() {
+      return sourceData.categories.find(category => category.id === this.id)
+    }
+  },
+
+  methods: {
+    getForumForCategory(category) {
+      return sourceData.forums.filter(forums => forums.categoryId === category.id)
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
