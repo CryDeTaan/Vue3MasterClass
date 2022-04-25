1f7cc7797b20a2472d07d2c26c66f26b6b48b5f6
CryDeTaan <CryDeTaan@users.noreply.github.com>
2022-04-25 20:10:39 +0100

    ✨ New Categories component.

---
+++ src/components/CategoryList.vue
@@ -0,0 +1,38 @@
+<template>
+  <forum-list
+    v-for="category in categories"
+    :key="category.id"
+    :forums="getForumForCategory(category)"
+    :category-name="category.name"
+  />
+</template>
+
+<script>
+import sourceData from '@/data.json'
+import ForumList from "@/components/ForumList";
+export default {
+  name: "CategoryList",
+  components: {
+    ForumList
+  },
+
+  props: {
+    categories: {
+      type: Array,
+    },
+  },
+
+  methods: {
+    getForumForCategory (category) {
+      return sourceData.forums.filter(forums => forums.categoryId === category.id )
+    }
+
+  }
+}
+
+
+</script>
+
+<style scoped>
+
+</style>
